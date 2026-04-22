25c0a3890269f92592d662320aa1ab74dd5a33dd
weetime <351075478@qq.com>
2026-04-22 20:34:39 +0800

    chore(api): remove Nest default scaffold (AppController/AppService/spec)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller.js";
-import { AppService } from "./app.service.js";
 import { HealthModule } from "./modules/health/health.module.js";
 import { DebugProxyModule } from "./modules/debug-proxy/debug-proxy.module.js";
 import { E2ETestModule } from "./modules/e2e-test/e2e-test.module.js";
@@ -8,7 +6,5 @@ import { LoadTestModule } from "./modules/load-test/load-test.module.js";
 
 @Module({
   imports: [HealthModule, DebugProxyModule, E2ETestModule, LoadTestModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
